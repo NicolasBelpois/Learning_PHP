7dc0a0a6c48717af9e1fbc6227e1fc86c55334de
Belpois Nicolas <nicolasbelpois@gmail.com>
2017-11-23 16:54:11 +0100

    sdfsdf

--- index.php
+++ index.php
@@ -1,4 +1,28 @@
 <?php
 
-echo "fuck !";
+
+try
+{
+    // On se connecte à MySQL
+    $pdo = new PDO('pgsql:host=ec2-46-137-174-67.eu-west-1.compute.amazonaws.com;dbname=d30lm4pll7pr08;', 'lkqmlvmezftqdz', '848d637e29fdce528968dda4b9844195ae30ee680b8fbe52dabbd1c9c8f07dac');
+}
+catch(Exception $e)
+{
+    // En cas d'erreur, on affiche un message et on arrête tout
+        die('Erreur : '.$e->getMessage());
+}
+$query = 'INSERT INTO nomtest (nom, description) VALUES (?, ?);';
+$prep = $pdo->prepare($query);
+
+$prep->bindValue(1, 'bertand', PDO::PARAM_STR);
+$prep->bindValue(2, 'ceci est un test pour desc', PDO::PARAM_STR);
+$prep->execute();
+$resultat = $pdo->query('SELECT * FROM nomtest');
+while ($donnees = $resultat->fetch())
+{
+  echo '<br/>';
+  echo $donnees['nom'];
+  echo ' : ';
+  echo $donnees['description'];
+}
  ?>
